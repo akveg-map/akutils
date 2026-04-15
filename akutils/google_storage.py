1deb1ecc9c2afda67286a06c0edaafd4b9a7ea18
Timm Nawrocki <twnawrocki@alaska.edu>
2026-04-15 13:16:50 -0800

    Add storage client
    Added storage client to google storage scripts.

--- akutils/google_storage.py
+++ akutils/google_storage.py
@@ -1,12 +1,12 @@
 # Define function to check if a file exists in GCS
-def gcs_blob_exists(gcs_uri):
+def gcs_blob_exists(gcs_uri, storage_client):
     bucket_name = gcs_uri.split('/')[2]
     blob_name = '/'.join(gcs_uri.split('/')[3:])
     bucket = storage_client.bucket(bucket_name)
     return bucket.blob(blob_name).exists()
 
 # Define function to download a file from GCS to local storage
-def download_from_gcs(gcs_uri, local_path):
+def download_from_gcs(gcs_uri, local_path, storage_client):
     bucket_name = gcs_uri.split('/')[2]
     blob_name = '/'.join(gcs_uri.split('/')[3:])
     bucket = storage_client.bucket(bucket_name)
@@ -14,14 +14,14 @@ def download_from_gcs(gcs_uri, local_path):
     blob.download_to_filename(local_path)
 
 # Define function to upload a file from local storage to GCS
-def upload_to_gcs(local_path, gcs_uri):
+def upload_to_gcs(local_path, gcs_uri, storage_client):
     bucket_name = gcs_uri.split('/')[2]
     blob_name = '/'.join(gcs_uri.split('/')[3:])
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
     blob.upload_from_filename(local_path)
 
-# Define a function to generate vsigs paths
-def get_vsi_paths(bucket_name, prefix):
+# Define a function to generate vsigs paths for tif rasters
+def get_vsi_paths(bucket_name, prefix, storage_client):
     blobs = storage_client.list_blobs(bucket_name, prefix=prefix)
     return [f'/vsigs/{bucket_name}/{b.name}' for b in blobs if b.name.endswith('.tif')]
